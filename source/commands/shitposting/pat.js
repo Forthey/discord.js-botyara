2ed40df5fefd555368d9ad790e5aa5ab403295b7
Forthey <0forthey0@gmail.com>
2024-02-13 14:11:02 +0300

    Add test subcommands

---
+++ source/commands/shitposting/pat.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Get info about a user or a server!')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('user')
+                .setDescription('Info about a user')
+                .addUserOption(option => option.setName('target').setDescription('The user')))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('server')
+                .setDescription('Info about the server')),
+    async execute(interaction) {
+        await interaction.reply("Похуй чо ты написал там");
+    },
+}
