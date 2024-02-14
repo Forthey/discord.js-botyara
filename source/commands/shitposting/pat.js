2693cea1c749b6a5a3e383f5d8ffc2234e1b8991
Forthey <0forthey0@gmail.com>
2024-02-15 00:13:35 +0300

    Fix pat so it atleast somehow works

--- source/commands/shitposting/pat.js
+++ source/commands/shitposting/pat.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const getPetGif = require("pet-pet-gif");
+const fs = require("fs");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,8 +12,13 @@ module.exports = {
                 .setDescription('кому проявить свою безграничную любовь')
                 .setRequired(true)),
     async execute(interaction) {
-        const user = interaction.options.getUser('user');
-        const animatedGif = await getPetGif(user.avatarURL());
-        await interaction.reply(animatedGif);
+        const user = await interaction.options.getUser('user');
+        let url = user.avatarURL();
+        url = url.substring(0, url.indexOf(".", url.length - 5)) + ".png";
+        console.log(url);
+        let animatedGif = await getPetGif(url);
+        const patGif = new AttachmentBuilder(animatedGif);
+        //await interaction.reply({ files : [patGif] });
+        await interaction.reply({files: [{attachment: animatedGif}]});
     },
 }
